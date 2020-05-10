a68d1d64a0412e0ca7afb5e8c8331e3c60f11533
Alena Hasslacher <alena.hasslacher@gmail.com>
2020-05-10 13:21:38 +0200

    Add post request w headers
    Text is input from command line, though the word count is not yet
    entirely correct

---
+++ index.js
@@ -0,0 +1,21 @@
+const got = require('got');
+
+(async () => {
+  try {
+    const response = await got.post(
+      'https://apidemo.theysay.io/api/v1/sentiment',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Referer: 'https://apidemo.theysay.io/',
+        },
+        json: {
+          text: `${process.argv.slice(2)}`, //shows correct word count when it's one word, and then oooh lala, when 2 - 3, when 3 - 5, when 4 - 7...
+        },
+      },
+    );
+    console.log(response.body);
+  } catch (error) {
+    console.log(error.response.body);
+  }
+})();
